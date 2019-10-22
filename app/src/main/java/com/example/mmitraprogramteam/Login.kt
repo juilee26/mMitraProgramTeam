c7bada2f041c613d25b33ac5d8ca40275c0117b7
juilee26 <juilee.shinde@inscripts.in>
2019-10-22 11:00:13 +0530

    splashScreen and loginScreen layout complete

---
+++ app/src/main/java/com/example/mmitraprogramteam/Login.kt
@@ -0,0 +1,12 @@
+package com.example.mmitraprogramteam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Login : AppCompatActivity (){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    setContentView(R.layout.layout_login)
+
+    }
+}
